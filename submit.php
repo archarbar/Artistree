b7ca03f328d291b07e2356e422549cda388965ef
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-09-13 07:17:23 -0400

    made final modifications to all the files so they work on a LAMP stack

--- submit.php
+++ submit.php
@@ -25,17 +25,17 @@ $artist= $_POST['artist'];
 <head> 
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<title>Results <?php ?></title>
+	<title>Results For <?php echo " ".ucfirst($artist); ?></title>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
 	<link rel="stylesheet" type="text/css" href="style.css"> 
 	<link rel="shortcut icon" href="img/favicon.png">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://d3js.org/d3.v6.min.js"></script>
+	<script src="https://d3js.org/d3.v6.min.js"></script> <!--d3.js which is a very popular data visualisation library that works with javascript -->
 </head>
 <body class="result">
 <a style="font-size:13px;font-family:Courier;margin-right:5px;margin-top:0px;position:absolute;color:white;right:0;" href="about.html">About Artistree</a>
-<a style="font-size:14px;font-family:Courier;margin-left:5px;margin-top:0px;position:absolute;color:white;" href="index.html">Artistree</a>
-<h4 align="center"> Artist:<?php echo " ".$artist;?></h4>
+<a style="font-size:14px;font-family:Courier;margin-left:10px;margin-top:0px;position:absolute;color:white;" href="index.html">Artistree</a>
+<h2 align="center"> Artist:<?php echo " ".ucfirst($artist);?></h2>
 <?php
 $output = shell_exec("python ./src/genius.py $artist 2>&1"); //change this to python3 when uploading to server
 ?>
@@ -44,17 +44,21 @@ $output = shell_exec("python ./src/genius.py $artist 2>&1"); //change this to py
 <svg></svg>
 </center>
 <script>
-raw=(<?php echo '`'.$output.'`' ?>).toString().replace(/\n/g," ");
-raw=raw.replace(/'/g, '"');
-console.log(raw);
-dataRaw=JSON.parse(raw) ;
+raw=(<?php echo '`'.$output.'`' ?>).toString().replace(/\n/g," "); //get data that was formatted by genius.py and replace newlines by spaces
+raw=raw.replace(/'/g, '"'); //replace single quotes by double quotes
+//console.log(raw); //debug
+dataRaw=JSON.parse(raw) ; //transform the JSON string into a javascript object to manipulate and read it more easily
 
-for (x in dataRaw) {
+//create an second object 
+//dataRaw -> parsed which will be the object used by d3.js, sinced the original python object is unusable.
+//d3.js is very picky in how it handles objects
+
+for (x in dataRaw) { //creating the main dictionnary with main artist
 	if(dataRaw[x][0]==0){
-		var parsed={name:x, count:dataRaw[x][0], url:dataRaw[x][1], children:[]};
+		var parsed={name:x, count:dataRaw[x][0], url:dataRaw[x][1], children:[]}; 
 	}
 }
-for (x in dataRaw) {
+for (x in dataRaw) { //creating the children dictionaries of the linked artists
 	if(dataRaw[x][0]!=0){
 		var subParsed={name:x, count:dataRaw[x][0], url:dataRaw[x][1], children:[]};
 		parsed.children.push(subParsed);
@@ -63,18 +67,21 @@ for (x in dataRaw) {
 
 
 
-const svg = d3.select('svg');
-const width= document.body.clientWidth;
-const height =  document.body.clientHeight;
-const margin= { top:0, right:110, bottom:0, left:90};
+const svg = d3.select('svg'); //getting svg element to change it using d3 functions
+const width= document.body.clientWidth; //getting window width
+var height =  document.body.clientHeight; //getting window height
+const margin= { top:0, right:150, bottom:50, left:20};
 const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+if (height < ((parsed.children).length-1)*130) { //depending on how many artists were found, if this surpasses a limit, we need to make the scrollable window longer 
+	height=((parsed.children).length-1)*130;
+} 
+const innerHeight= height - margin.bottom - margin.top; 
 const treeLayout= d3.tree().size([innerHeight, innerWidth]);
 
 	
 const g = svg 
 	.attr('width', width)
-	.attr('height', height)
+	.attr('height',height)
 	.append('g')
 		.attr('transform', `translate(${margin.left},${margin.top})`);
 			 
@@ -99,17 +106,17 @@ g.selectAll('text').data(root.descendants())
 		.attr('x', d => d.y)
 		.attr('dy', '0.32em')
 		.attr('font-size', d => 2/((d.depth+1))+0.2+'em')
-		.attr('text-anchor', 'middle')
+		.attr('text-anchor', d=> d.depth? 'end':'start')
 		.text(d=> d.depth ? d.data.name+": "+d.data.count : d.data.name);	
 
 
 g.selectAll('image').data(root.descendants())
 	.enter().append('image')
 		.attr('xlink:href', d => d.data.url)
-		.attr('y', d => d.x+15/((d.depth+1)))
-		.attr('x', d => d.y)
-		.attr("width", d => 150/((d.depth+1))+"px")
-		.attr("height", "70px");
+		.attr('y', d => d.depth? d.x-36:d.x+20)
+		.attr('x', d => d.depth? d.y+10:d.y)
+		.attr("width", d => d.depth? "80px":"150px")
+		.attr("height", d => d.depth? "80px":"150px");
 		
 
 //window.scrollTo(0,document.body.scrollHeight);
