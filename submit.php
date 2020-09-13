1c1224b84e6951289642b7029b4aa8deca275506
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-09-13 02:41:24 -0400

    I took the python files that Victor made and made them work with a lamp server. Modified the python files a little. Modified submit.php for it to take the output from the python file and work with it

--- submit.php
+++ submit.php
@@ -1,12 +1,3 @@
-<html>
-<head> 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<title>Results <?php ?></title>
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> <!--Link to boostrap to make a professional submit page -->	
-	<link rel="stylesheet" type="text/css" href="style.css"> 
-	<link rel="shortcut icon" href="img/favicon.png">
-</head>
-<body class="result">
 <?php
 //Down below template if we need to connect to a database
 /*
@@ -28,32 +19,104 @@ echo $data[0]["count"];
 */
 
 $artist= $_POST['artist'];
-$output = shell_exec("python myscript.py $artist 2>&1"); //change this to python3 when uploading to server
-echo "<h3>".$output."</h3>";
 ?>
-<script src="https://d3js.org/d3.v6.min.js"></script> <!--Import d3.js -->
+
+<html>
+<head> 
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Results <?php ?></title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<link rel="shortcut icon" href="img/favicon.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://d3js.org/d3.v6.min.js"></script>
+</head>
+<body class="result">
+<a style="font-size:13px;font-family:Courier;margin-right:5px;margin-top:0px;position:absolute;color:white;right:0;" href="about.html">About Artistree</a>
+<a style="font-size:14px;font-family:Courier;margin-left:5px;margin-top:0px;position:absolute;color:white;" href="index.html">Artistree</a>
+<h4 align="center"> Artist:<?php echo " ".$artist;?></h4>
+<?php
+$output = shell_exec("python ./src/genius.py $artist 2>&1"); //change this to python3 when uploading to server
+?>
+
+<center>
+<svg></svg>
+</center>
 <script>
+raw=(<?php echo '`'.$output.'`' ?>).toString().replace(/\n/g," ");
+raw=raw.replace(/'/g, '"');
+console.log(raw);
+dataRaw=JSON.parse(raw) ;
+
+for (x in dataRaw) {
+	if(dataRaw[x][0]==0){
+		var parsed={name:x, count:dataRaw[x][0], url:dataRaw[x][1], children:[]};
+	}
+}
+for (x in dataRaw) {
+	if(dataRaw[x][0]!=0){
+		var subParsed={name:x, count:dataRaw[x][0], url:dataRaw[x][1], children:[]};
+		parsed.children.push(subParsed);
+	}
+}
+
+
+
 const svg = d3.select('svg');
-const width=document.body.clientWidth;
-const height = document.body.clientHeight;
-//tree is a function from d3 that creates nodes and links
-const treeLayout= d3.tree()
-	.size([height, width]);
+const width= document.body.clientWidth;
+const height =  document.body.clientHeight;
+const margin= { top:0, right:110, bottom:0, left:90};
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
+const treeLayout= d3.tree().size([innerHeight, innerWidth]);
+
 	
-svg 
+const g = svg 
 	.attr('width', width)
 	.attr('height', height)
-  
+	.append('g')
+		.attr('transform', `translate(${margin.left},${margin.top})`);
+			 
   
 //promise for loading the json file. Only now for development. Then the json will be send through python
-d3.json('artists.json')
-	.then(data => {
-		const root = hierarchy(data);
-		treeLayout(root)
-		console.log(data);
-	});
-	
-	
+
+data=parsed;
+const root = d3.hierarchy(data);
+const links = treeLayout(root).links();
+const linkPathGenerator = d3.linkHorizontal()
+	.x(d => d.y)
+	.y(d => d.x);
+
+g.selectAll('path').data(links)
+	.enter().append('path')
+		.attr('d', linkPathGenerator);
+		
+		
+g.selectAll('text').data(root.descendants())
+	.enter().append('text')
+		.attr('y', d => d.x)
+		.attr('x', d => d.y)
+		.attr('dy', '0.32em')
+		.attr('font-size', d => 2/((d.depth+1))+0.2+'em')
+		.attr('text-anchor', 'middle')
+		.text(d=> d.depth ? d.data.name+": "+d.data.count : d.data.name);	
+
+
+g.selectAll('image').data(root.descendants())
+	.enter().append('image')
+		.attr('xlink:href', d => d.data.url)
+		.attr('y', d => d.x+15/((d.depth+1)))
+		.attr('x', d => d.y)
+		.attr("width", d => 150/((d.depth+1))+"px")
+		.attr("height", "70px");
+		
+
+//window.scrollTo(0,document.body.scrollHeight);
+
+
+</script>
+<script>
 
 </script>
 
